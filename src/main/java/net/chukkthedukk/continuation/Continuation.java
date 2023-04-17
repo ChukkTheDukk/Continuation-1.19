a732a42883870e46fbdbd1f2954578a9b6ed7f04
ChukkTheDukk <ryanmatthewstevenson@gmail.com>
2023-04-17 00:10:02 -0600

    Commit

---
+++ src/main/java/net/chukkthedukk/continuation/Continuation.java
@@ -0,0 +1,16 @@
+package net.chukkthedukk.continuation;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+//very important comment
+public class Continuation implements ModInitializer {
+	public static final String MOD_ID = "continuation";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/chukkthedukk/continuation/ContinuationClient.java
@@ -0,0 +1,10 @@
+package net.chukkthedukk.continuation;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ContinuationClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
